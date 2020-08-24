7d51cd6ded5f31102bfb8334afe29809775ea136
Dave Letorey <daveletorey@gmail.com>
2020-08-24 16:40:57 +0100

    completed exercise 29. Negative Indexing in Strings

--- Python Programming Masterclass/Python/HelloWorld/stringsTwo.py
+++ Python Programming Masterclass/Python/HelloWorld/stringsTwo.py
@@ -1,3 +1,4 @@
+#         43210987654321
 parrot = "Norwegian Blue"
 print(parrot)
 print(parrot[3])
@@ -6,3 +7,18 @@ print(parrot[9])
 print(parrot[3])
 print(parrot[6])
 print(parrot[8])
+
+print()
+print(parrot[-11])
+print(parrot[-1])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+print()
+print(parrot[3 - 14])
+print(parrot[4 - 14])
+print(parrot[9 - 14])
+print(parrot[3 - 14])
+print(parrot[6 - 14])
+print(parrot[8 - 14])
