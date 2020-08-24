93cb4bc18891c221148b4c06530ec16a52c2dae8
Dave Letorey <daveletorey@gmail.com>
2020-08-24 15:39:50 +0100

    completed exercise 22 Variables and Types

--- Python Programming Masterclass/Python/HelloWorld/Strings.py
+++ Python Programming Masterclass/Python/HelloWorld/Strings.py
@@ -4,8 +4,16 @@ print("Python's strings are easy to use")
 print('We can even include "quotes" in strings')
 print('hello' + ' world')
 greeting = "Hello"
-name = input("please enter your name ")
+name = "Dave"
 
 # if we want we can add a space in here
 
 print(greeting + " " + name)
+age = 24
+print(age)
+print(type(greeting))
+print(type(age))
+
+age = "2 years"
+print(age)
+print(type(age))
