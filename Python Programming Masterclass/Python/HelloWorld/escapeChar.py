4b74609b7a50a308cb11613f86125b1555c14fff
Dave Letorey <daveletorey@gmail.com>
2020-08-24 15:25:48 +0100

    completed exercise 20 escaping charaters in strings

---
+++ Python Programming Masterclass/Python/HelloWorld/escapeChar.py
@@ -0,0 +1,14 @@
+splitString = "This string has been \nsplit over \nlines"
+print(splitString)
+
+tabString = "1\t2\t3\t4\t5"
+print(tabString)
+print('The pet shop owner said "No, no, \'e\'s uh,…he\'s resting".')
+print("The pet shop owner said \"No, no, 'e's uh,…he's resting\".")
+print("""The pet shop owner said "No, no, 'e's uh,…he's resting".""")
+print('''The pet shop owner said "No, no, 'e's uh,…he's resting".''')
+anotherSplitString = '''This string has been
+split over
+several \
+lines'''
+print(anotherSplitString)
