5de62c3ca04c8e94ddff0f008a273cf127244aed
Dave Letorey <daveletorey@gmail.com>
2020-08-24 18:53:33 +0100

    completed 39 python 2 interpolation

---
+++ Python Programming Masterclass/Python/HelloWorld/interpolation.py
@@ -0,0 +1,4 @@
+age = 50
+print("My age is %d years" % age) # %d says we're going to add in a decimal number you can also use %f for a float %s for a string
+major = "years"
+print("My age is %d %s %d %s" % (age, major, 11, "months")) # here we have 2 decimal numbers and 2 strings one string form a variable
