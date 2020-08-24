e9323fd24aaf54c18656d16135549ccbf3c9e96a
Dave Letorey <daveletorey@gmail.com>
2020-08-24 17:37:19 +0100

    completed exercise 33 slicing backwards

---
+++ Python Programming Masterclass/Python/HelloWorld/sliceBack.py
@@ -0,0 +1,13 @@
+#          12345678901234567890123456
+#          65432109876543210987654321
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[25::-1]
+tebahpla = letters[::-1] # as the step is a negative this works as python reverses the thestart and end characters
+print(backwards)
+print(tebahpla)
+print(letters[-10:-13:-1])
+print(letters[16:13:-1])
+print(letters[-22::-1])
+print(letters[4::-1])
+print(letters[:-9:-1])
