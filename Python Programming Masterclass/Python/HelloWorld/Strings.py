98d922976742e7e81ae73338156a56acd895d9f7
Dave Letorey <daveletorey@gmail.com>
2020-08-24 15:24:56 +0100

    completed exercise 19 Strings in Python

---
+++ Python Programming Masterclass/Python/HelloWorld/Strings.py
@@ -0,0 +1,11 @@
+print('Today is a good day to learn Python')
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print('hello' + ' world')
+greeting = "Hello"
+name = input("please enter your name ")
+
+# if we want we can add a space in here
+
+print(greeting + " " + name)
