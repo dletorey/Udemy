a76cc1e5807d6222bccd554583de6fac89e38bf4
Dave Letorey <daveletorey@gmail.com>
2020-08-25 15:17:27 +0100

    completed exercise 43 elif

--- Python Programming Masterclass/Python/FlowControl/blocks.py
+++ Python Programming Masterclass/Python/FlowControl/blocks.py
@@ -3,18 +3,30 @@
 #    print("*" * 80)
 
 # Exercise 42 if Statements
+#name = input("Please enter your name: ")
+#age = int(input("How old are you, {0}? ".format(name)))
+#print(age)
+## Checking 18 or older
+#if age >= 18:
+#    print("You are old enough to vote")
+#    print("Please put an \"X\" in the box")
+#else:
+#    print("Please come back in {0} years".format(18 - age))
+## reversed checking if younger than 18
+#if age < 18:
+#    print("Please come back in {0} years".format(18 - age))
+#else:
+#    print("You are old enough to vote")
+#    print("Please put an \"X\" in the box")
+
+# Exercise 43 elif
 name = input("Please enter your name: ")
 age = int(input("How old are you, {0}? ".format(name)))
 print(age)
-# Checking 18 or older
-if age >= 18:
-    print("You are old enough to vote")
-    print("Please put an \"X\" in the box")
-else:
-    print("Please come back in {0} years".format(18 - age))
-# reversed checking if younger than 18
 if age < 18:
     print("Please come back in {0} years".format(18 - age))
+elif age == 900: # this is equivalent to else if
+    print("Sorry Yoda you die in Return of the Jedi")
 else:
     print("You are old enough to vote")
     print("Please put an \"X\" in the box")
