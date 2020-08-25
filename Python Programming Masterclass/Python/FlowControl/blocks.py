a48def3ac1c37a0ccdc1446bb73908c9d3ad8bab
Dave Letorey <daveletorey@gmail.com>
2020-08-25 15:10:30 +0100

    completed exercise 42 if Statements

--- Python Programming Masterclass/Python/FlowControl/blocks.py
+++ Python Programming Masterclass/Python/FlowControl/blocks.py
@@ -1,3 +1,20 @@
-for i in range(1, 13):
-    print("Number {} squared is {} and cubed is {:4}".format(i, i ** 2, i ** 3))
-    print("*" * 80)
+#for i in range(1, 13):
+#    print("Number {} squared is {} and cubed is {:4}".format(i, i ** 2, i ** 3))
+#    print("*" * 80)
+
+# Exercise 42 if Statements
+name = input("Please enter your name: ")
+age = int(input("How old are you, {0}? ".format(name)))
+print(age)
+# Checking 18 or older
+if age >= 18:
+    print("You are old enough to vote")
+    print("Please put an \"X\" in the box")
+else:
+    print("Please come back in {0} years".format(18 - age))
+# reversed checking if younger than 18
+if age < 18:
+    print("Please come back in {0} years".format(18 - age))
+else:
+    print("You are old enough to vote")
+    print("Please put an \"X\" in the box")
