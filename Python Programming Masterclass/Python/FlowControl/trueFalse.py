c3418a91e2bd24ebc0ce4a43e550443fd5cda97a
Dave Letorey <daveletorey@gmail.com>
2020-08-25 17:37:24 +0100

    completed 53 truthy valies

--- Python Programming Masterclass/Python/FlowControl/trueFalse.py
+++ Python Programming Masterclass/Python/FlowControl/trueFalse.py
@@ -1,8 +1,28 @@
-day = "Saturday"
-temperature = 30
-raining = False
+# day = "Saturday"
+# temperature = 30
+# raining = False
+#
+# if (day == "Saturday" and temperature > 27) and not raining: # the brackets show precedence
+#     print("Go swimming")
+# else:
+#     print("Learn Python")
 
-if (day == "Saturday" and temperature > 27) and not raining: # the brackets show precedence
-    print("Go swimming")
+# exercise 53 Truthy Values
+
+if 0:
+    print("True") #this will never happen because in a condition 0 will always resolve to False
 else:
-    print("Learn Python")
+    print("Flase")
+
+name = input("Please enter your name: ")
+if name:
+    print("Hello, {}".format(name))
+else:
+    print("are you the man with no name")
+
+#compare to empty string
+name2 = input("Please enter your name: ")
+if name2 != "":
+    print("Hello, {}".format(name2))
+else:
+    print("are you the man with no name")
