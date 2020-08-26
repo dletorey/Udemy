6ad26ac620ca120c8f0359984049f3460137a654
Dave Letorey <daveletorey@gmail.com>
2020-08-26 16:17:33 +0100

    completed exercise 58 stepping over for loops

--- Python Programming Masterclass/Python/FlowControl/forLopps.py
+++ Python Programming Masterclass/Python/FlowControl/forLopps.py
@@ -1,4 +1,18 @@
-parrot = "Norwegian Blue"
+# # Exercise 57 for Loops
+# parrot = "Norwegian Blue"
+#
+# for character in parrot:
+#     print(character)
 
-for character in parrot:
-    print(character)
+#Exercise 58 Stepping through a for Loop
+number = "9,423;456:292,747;327:272 849,939;535"
+seperators = ""
+
+for char in number:
+    if not char.isnumeric():
+        seperators = seperators + char
+
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
