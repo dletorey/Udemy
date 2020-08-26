759ec2ecb0b53407240d97c0882b6f394521516b
Dave Letorey <daveletorey@gmail.com>
2020-08-26 16:48:54 +0100

    updated condition to use range instead of checking the values

--- Python Programming Masterclass/Python/FlowControl/ranges.py
+++ Python Programming Masterclass/Python/FlowControl/ranges.py
@@ -7,14 +7,31 @@
 # for i in range(0, 10):
 #     print(i)
 
-#Exercise 61 more about ranges
+# #Exercise 61 more about ranges
+# # Part 1
+# for i in range(10): # if you want to start at 0 then you can just add the end value
+#     print("i is now {}".format(i))
+# print("-" * 10)
+# for i in range(0, 10, 2): # if you want to step then you need to include a start value
+#     print("i is now {}".format(i))
+# print("-" * 10)
+# for i in range(10, -1, -1): # if you want to go backwards higher number first and then a negative step value
+#     print("i is now {}".format(i))
+# print("-" * 10)
 
-for i in range(10): # if you want to start at 0 then you can just add the end value
-    print("i is now {}".format(i))
-print("-" * 10)
-for i in range(0, 10, 2): # if you want to step then you need to include a start value
-    print("i is now {}".format(i))
-print("-" * 10)
-for i in range(10, -1, -1): # if you want to go backwards higher number first and then a negative step value
-    print("i is now {}".format(i))
-print("-" * 10)
+#Part 2
+age = int(input("How old are you? "))
+
+# if age >= 16 and age <= 65:
+# if 16 <= age <= 65:
+if age in range(16, 66):
+    print("Have a good day at work")
+else:
+    print("Enjoy your free time")
+
+print("-" * 80)
+
+if age < 16 or age > 65:
+    print("Enjoy your free time")
+else:
+    print("Have a good day at work")
