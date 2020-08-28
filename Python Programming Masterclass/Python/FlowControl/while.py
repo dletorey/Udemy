6f351b96dbe16032052e673a44c02d333aa2a591
Dave Letorey <daveletorey@gmail.com>
2020-08-28 11:36:06 +0100

    completed exercise 66 while loops

---
+++ Python Programming Masterclass/Python/FlowControl/while.py
@@ -0,0 +1,11 @@
+print("using a for loop")
+for i in range(10):
+    print("i is now {}".format(i))
+print("-" * 15)
+
+print("using a while loop")
+i = 0
+while i < 10:
+    print("i is now {}".format(i))
+    i += 1 # this is the same as i = i + 1 but is preferential
+print("-" * 15)
