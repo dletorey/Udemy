437fab0124d30cba6fac631421bffb49990ef92f
Dave Letorey <daveletorey@gmail.com>
2020-08-28 12:41:33 +0100

    completed Challenge in exercise 69

--- Python Programming Masterclass/Python/FlowControl/guessingGame.py
+++ Python Programming Masterclass/Python/FlowControl/guessingGame.py
@@ -1,20 +1,38 @@
-answer = 5
-print("Please guess a number between 1 & 10: ")
+import random #imports the random module
+
+highest = 10 # sets the highest possible number and makes it easy to change in one spot, here
+answer = random.randint(1, highest) # generates a random nuber between 1 and highest using randint from the random module imported
+print("the correct answer is {}".format(answer)) # TODO remove this later this is only for testing
+print("Please guess a number between 1 & {}, to quit type 0: ".format(highest))
 guess = int(input())
 
-# Exercise 48 Challenge
-if guess == answer:
-    print("You guessed it first time")
-else:
-    if guess < answer:
+#Exercise 69 Challenge
+while guess != answer:
+    if guess == 0:
+        break
+    elif guess < answer:
         print("Guess Higher")
-    else:
-        print("guess lower")
-    guess = int(input())
-    if guess == answer:
-        print("2nd time lucky")
-    else:
-        print("Failure")
+    elif guess > answer:
+        print("Guess Lower")
+    guess = int(input("Keep guessing or use 0 to quit "))
+if guess == 0:
+    print("You quit the game")
+else:
+    print("you guessed the correct answer")
+
+# # Exercise 48 Challenge
+# if guess == answer:
+#     print("You guessed it first time")
+# else:
+#     if guess < answer:
+#         print("Guess Higher")
+#     else:
+#         print("guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("2nd time lucky")
+#     else:
+#         print("Failure")
 
 # print("Please guess a number between 1 & 10: ")
 # guess = int(input())
