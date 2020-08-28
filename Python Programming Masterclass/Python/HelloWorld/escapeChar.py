5c93a187e2e87d8773bbf02a22d7cdd4cb1fa6fa
Dave Letorey <daveletorey@gmail.com>
2020-08-28 14:47:13 +0100

    refactored the variable names to conform with PEP8

--- Python Programming Masterclass/Python/HelloWorld/escapeChar.py
+++ Python Programming Masterclass/Python/HelloWorld/escapeChar.py
@@ -1,16 +1,16 @@
-splitString = "This string has been \nsplit over \nlines"
-print(splitString)
+split_string = "This string has been \nsplit over \nlines"
+print(split_string)
 
-tabString = "1\t2\t3\t4\t5"
-print(tabString)
+tab_string = "1\t2\t3\t4\t5"
+print(tab_string)
 print('The pet shop owner said "No, no, \'e\'s uh,…he\'s resting".')
 print("The pet shop owner said \"No, no, 'e's uh,…he's resting\".")
 print("""The pet shop owner said "No, no, 'e's uh,…he's resting".""")
 print('''The pet shop owner said "No, no, 'e's uh,…he's resting".''')
-anotherSplitString = '''This string has been
+another_split_string = '''This string has been
 split over
 several \
 lines'''
-print(anotherSplitString)
+print(another_split_string)
 print("C:\\Users\\tim\\notes.txt")
 print(r"C:\Users\tim\notes.txt")
