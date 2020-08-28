dc32b1acf6dab2f2d6a96240105edbda2af7578f
Dave Letorey <daveletorey@gmail.com>
2020-08-28 11:56:14 +0100

    completed Coding Exercise 10 break

---
+++ Python Programming Masterclass/Python/FlowControl/coding_exercises.py
@@ -0,0 +1,6 @@
+# Coding Exercise 10 break
+# Modify the code inside this loop to stop when i is exactly divisible by 11
+for i in range(0, 100, 7):
+    print(i)
+    if i > 0 and i % 11 == 0:
+        break
