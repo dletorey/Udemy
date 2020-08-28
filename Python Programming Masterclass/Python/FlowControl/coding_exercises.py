bc97f4648b9c267a1eff3c1c690357cabd3aad4f
Dave Letorey <daveletorey@gmail.com>
2020-08-28 14:26:04 +0100

    completed Coding Exercise 12 augmented assignment in a loop

--- Python Programming Masterclass/Python/FlowControl/coding_exercises.py
+++ Python Programming Masterclass/Python/FlowControl/coding_exercises.py
@@ -15,3 +15,20 @@ for i in range(1, 20):
         continue
     else:
         print(i)
+
+# Coding Exercise 12 augmented assignment in a loop
+# Early computers could only perform addition. In order to multiply one number by another, they performed repeated addition.
+# For example, 5 * 8 was performed by adding 5 eight times
+# 5 + 5 + 5 + 5 + 5 + 5 + 5 + 5 = 40
+# Use a for loop, and augmented assignment, to give answer the result of multiplying number by multiplier
+# Paste your code into your IDE, and make sure it works with different values for number & multiplier
+# Note that this exercise checking system will only validate your code with the values 5 and 8, for number and multiplier.
+
+number = 5
+multiplier = 8
+answer = 0
+
+# add your loop after this comment
+for num in range(multiplier):
+    answer += number
+print(answer)
