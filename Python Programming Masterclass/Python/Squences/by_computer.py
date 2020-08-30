dd14dde41a2ed0ff7f48ee56890487881e117d3f
Dave Letorey <daveletorey@gmail.com>
2020-08-30 18:43:56 +0100

    completed exercise 97 improving code

--- Python Programming Masterclass/Python/Squences/by_computer.py
+++ Python Programming Masterclass/Python/Squences/by_computer.py
@@ -4,25 +4,36 @@ available_parts = ["computer",
                    "mouse",
                    "mouse mat",
                    "hdmi cable",
-                   "dvd drive"]
+                   "dvd drive",
+                   "track pad",
+                   "printer"]
+# valid_choices = [str(i) for i in range(1, len(available_parts) + 1)]
+valid_choices = []
+for i in range(1, len(available_parts) + 1):
+    valid_choices.append(str(i))
+print(valid_choices)
 current_choice = "-"
 computer_parts = [] # create an empty list to store the items
 
 while current_choice != '0':
-    if current_choice in "123456":
-        print("Adding {}, to computer shopping list".format(current_choice))
-        if current_choice == "1":
-            computer_parts.append("computer")
-        elif current_choice == "2":
-            computer_parts.append("monitor")
-        elif current_choice == "3":
-            computer_parts.append("keyboard")
-        elif current_choice == "4":
-            computer_parts.append("mouse")
-        elif current_choice == "5":
-            computer_parts.append("mousemat")
-        elif current_choice == "6":
-            computer_parts.append("hdmi cable")
+    if current_choice in valid_choices:
+        index = int(current_choice) -1
+        chosen_part = available_parts[index]
+        computer_parts.append(chosen_part)
+        print("Adding {}, to computer shopping list".format(chosen_part))
+        # print("Adding {}, to computer shopping list".format(current_choice))
+        # if current_choice == "1":
+        #     computer_parts.append("computer")
+        # elif current_choice == "2":
+        #     computer_parts.append("monitor")
+        # elif current_choice == "3":
+        #     computer_parts.append("keyboard")
+        # elif current_choice == "4":
+        #     computer_parts.append("mouse")
+        # elif current_choice == "5":
+        #     computer_parts.append("mousemat")
+        # elif current_choice == "6":
+        #     computer_parts.append("hdmi cable")
     else:
         print("Please add options from the list below:")
         for number, part in enumerate(available_parts):
