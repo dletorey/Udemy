941615ecc25a94c7010aa09f23f037c7d43e5201
Dave Letorey <daveletorey@gmail.com>
2020-08-30 12:09:21 +0100

    completed exercise 91 common sequnce operations

---
+++ Python Programming Masterclass/Python/Squences/number_lists.py
@@ -0,0 +1,12 @@
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+print(min(even))
+print(max(even))
+print(min(odd))
+print(max(odd))
+print("-" * 6)
+print(len(even))
+print(len(odd))
+
+print("-" * 6)
+print("mississippi".count("s"))
