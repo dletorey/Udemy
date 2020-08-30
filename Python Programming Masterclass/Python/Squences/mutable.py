f1b69621eb130ddab7cc9d9d494aad0f7ab2d0c2
Dave Letorey <daveletorey@gmail.com>
2020-08-30 12:02:52 +0100

    completed exercise 90 binging multiple names to a list

--- Python Programming Masterclass/Python/Squences/mutable.py
+++ Python Programming Masterclass/Python/Squences/mutable.py
@@ -11,3 +11,12 @@ print(id(another_list))
 shopping_list += ["cookies"]
 print(shopping_list)
 print(id(shopping_list))
+print(another_list)
+
+a = b = c = d = e = f = another_list
+
+print(a)
+print("Adding cream")
+b.append("Cream")
+print(c)
+print(d)
