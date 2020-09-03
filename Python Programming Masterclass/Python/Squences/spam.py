145d322d34d1cc5907dcb4f37e0c46f813fb3881
Dave Letorey <daveletorey@gmail.com>
2020-09-03 16:33:57 +0100

    completed exercise 115 processing nested lists

--- Python Programming Masterclass/Python/Squences/spam.py
+++ Python Programming Masterclass/Python/Squences/spam.py
@@ -7,3 +7,11 @@ menu = [
         ["spam", "bacon", "sausage", "spam"],
         ["spam", "egg", "spam", "spam", "bacon", "tomato", "spam"], # the last comma will be ignored but adding more items means to the list is eaiser and the git update would only mention the extra lines in the commit not the line with added comma
 ]
+for meal in menu:
+        if "spam" not in meal:
+                print(meal)
+                for  item in meal:
+                        print(item)
+        else:
+                print("{0} has a spam score of {1}"
+                      .format(meal, meal.count("spam")))
