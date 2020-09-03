3bfb69f3876496bfc39d5ced6aa4d8376a70c270
Dave Letorey <daveletorey@gmail.com>
2020-09-03 14:23:00 +0100

    completed exercise 106 safely removing values from the list

--- Python Programming Masterclass/Python/Squences/outliers.py
+++ Python Programming Masterclass/Python/Squences/outliers.py
@@ -1,14 +1,31 @@
-data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
-        160, 170, 183, 185, 187, 188, 191, 350, 360]
-# del data[0:2]
-# print(data)
-# del data[14:]
+# data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
+#         160, 170, 183, 185, 187, 188, 191, 350, 360]
+# # del data[0:2]
+# # print(data)
+# # del data[14:]
+# # print(data)
+#
+# min_valid = 100
+# max_valid = 200
+#
+# for index, value in enumerate(data):
+#     if (value < min_valid) or (value > max_valid):
+#         del data[index]
 # print(data)
 
+# exercise 106 safely removing items from a list
+data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
+        160, 170, 183, 185, 187, 188, 191, 350, 360]
 min_valid = 100
 max_valid = 200
 
+# process low values in list
+
+stop = 0
 for index, value in enumerate(data):
-    if (value < min_valid) or (value > max_valid):
-        del data[index]
+    if value >= min_valid:
+        stop = index
+        break
+print(stop) # for debugging
+del data[:stop]
 print(data)
