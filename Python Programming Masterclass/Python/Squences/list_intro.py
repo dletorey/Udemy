739db7fb7fdd2244e438abf98656fc4a2a19af58
Dave Letorey <daveletorey@gmail.com>
2020-09-03 13:03:57 +0100

    completed exercise 104 the documentation

--- Python Programming Masterclass/Python/Squences/list_intro.py
+++ Python Programming Masterclass/Python/Squences/list_intro.py
@@ -1,15 +1,28 @@
+# computer_parts = ["computer",
+#                   "monitor",
+#                   "keyboard",
+#                   "mouse",
+#                   "mouse mat"
+#                   ]
+# for part in computer_parts:
+#     print(part)
+#
+# print()
+# print(computer_parts[2])
+#
+# print()
+# print(computer_parts[0:3])
+# print(computer_parts[-1])
+
+# Exrcise 104 The documentation
 computer_parts = ["computer",
                   "monitor",
                   "keyboard",
                   "mouse",
                   "mouse mat"
                   ]
-for part in computer_parts:
-    print(part)
+print(computer_parts)
+print(computer_parts[3:])
 
-print()
-print(computer_parts[2])
-
-print()
-print(computer_parts[0:3])
-print(computer_parts[-1])
+computer_parts[3:] = ["trackball"] # the word trackball is put into a list otherwise each letter is added indivdually to the list
+print(computer_parts)
