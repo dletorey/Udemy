b7a43efa24a07b5c59851ea47a96b4c9a2af1c36
Dave Letorey <daveletorey@gmail.com>
2020-09-03 16:41:17 +0100

    completed exercise 116 nospam solution

---
+++ Python Programming Masterclass/Python/Squences/nospam.py
@@ -0,0 +1,21 @@
+menu = [
+    ["egg", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["egg", "bacon", "spam"],
+    ["egg", "bacon", "sausage", "spam"],
+    ["spam", "bacon", "sausage", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon", "tomato", "spam"], # the last comma will be ignored but adding more items means to the list is eaiser and the git update would only mention the extra lines in the commit not the line with added comma
+]
+# my solution to no spam
+for meal in menu:
+    for item in meal:
+        if "spam" != item:
+            print(item)
+    print()
+# solution 1
+for meal in menu:
+    for index in range(len(meal) -1, -1, -1):
+        if meal[index] == "spam":
+            del meal[index]
+    print(meal)
