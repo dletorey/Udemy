1173c51801356f4551f0067c297437ab21d74e0a
Dave Letorey <daveletorey@gmail.com>
2020-09-03 16:12:30 +0100

    completed exercise 114 Code Style

---
+++ Python Programming Masterclass/Python/Squences/spam.py
@@ -0,0 +1,9 @@
+menu = [
+        ["egg", "bacon"],
+        ["egg", "sausage", "bacon"],
+        ["egg", "spam"],
+        ["egg", "bacon", "spam"],
+        ["egg", "bacon", "sausage", "spam"],
+        ["spam", "bacon", "sausage", "spam"],
+        ["spam", "egg", "spam", "spam", "bacon", "tomato", "spam"], # the last comma will be ignored but adding more items means to the list is eaiser and the git update would only mention the extra lines in the commit not the line with added comma
+]
