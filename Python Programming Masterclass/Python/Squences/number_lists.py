f267e210d6592d91df18156c836fb5fdbdd34b90
Dave Letorey <daveletorey@gmail.com>
2020-09-03 12:36:08 +0100

    completed exercises 100 & 101

--- Python Programming Masterclass/Python/Squences/number_lists.py
+++ Python Programming Masterclass/Python/Squences/number_lists.py
@@ -1,12 +1,20 @@
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
-print(min(even))
-print(max(even))
-print(min(odd))
-print(max(odd))
-print("-" * 6)
-print(len(even))
-print(len(odd))
+# print(min(even))
+# print(max(even))
+# print(min(odd))
+# print(max(odd))
+# print("-" * 6)
+# print(len(even))
+# print(len(odd))
+#
+# print("-" * 6)
+# print("mississippi".count("s"))
 
-print("-" * 6)
-print("mississippi".count("s"))
+# exercise 99 sorting lists
+even.extend(odd)
+print(even)
+even.sort()
+print(even)
+even.sort(reverse=True)
+print(even)
