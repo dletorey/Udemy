19c247bbae88f3376b2231bb6eab500e8519469b
Dave Letorey <daveletorey@gmail.com>
2020-09-04 16:12:53 +0100

    completed exercise 119 join method

---
+++ Python Programming Masterclass/Python/Squences/joining_things.py
@@ -0,0 +1,15 @@
+flowers = [
+    "Daffodil",
+    "Crocus",
+    "Iris",
+    "Tulip",
+    "Rose",
+    "Lily",
+]
+# for flower in flowers:
+#     print(flower)
+
+separator = " | "
+output = separator.join(flowers)
+print(output)
+print(",".join(flowers))
--- Python Programming Masterclass/Python/Squences/nospam.py
+++ Python Programming Masterclass/Python/Squences/nospam.py
@@ -7,15 +7,15 @@ menu = [
     ["spam", "bacon", "sausage", "spam"],
     ["spam", "egg", "spam", "spam", "bacon", "tomato", "spam"], # the last comma will be ignored but adding more items means to the list is eaiser and the git update would only mention the extra lines in the commit not the line with added comma
 ]
-# my solution to no spam
-for meal in menu:
-    for item in meal:
-        if "spam" != item:
-            print(item, end=' ')
-    print()
+# # my solution to no spam
+# for meal in menu:
+#     for item in meal:
+#         if "spam" != item:
+#             print(item, end=' ')
+#     print()
 # solution 1
 for meal in menu:
     for index in range(len(meal) -1, -1, -1):
         if meal[index] == "spam":
             del meal[index]
-    print(meal)
+    print(", ".join(meal))
