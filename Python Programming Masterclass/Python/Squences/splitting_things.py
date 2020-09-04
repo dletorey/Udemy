e0dcbc1f81408d1de580645256c36605aff5c9a7
Dave Letorey <daveletorey@gmail.com>
2020-09-04 16:58:12 +0100

    added code to generate numbers for the challenge

--- Python Programming Masterclass/Python/Squences/splitting_things.py
+++ Python Programming Masterclass/Python/Squences/splitting_things.py
@@ -6,6 +6,3 @@ print(words)
 numbers = "9,234,5343,2214,32,324,23423,54"
 print(numbers.split(","))
 seperators = ","
-generated_list = "".join(char if char not in seperators else " " for char in numbers).split()
-print(generated_list)
-print([int(val) for val in generated_list])
