b0122fb8466f4c47cab931afb8ec5708065a8eb9
Dave Letorey <daveletorey@gmail.com>
2020-09-07 16:45:29 +0100

    printing out selected choice

--- Python Programming Masterclass/Python/Squences/jukebox_menu.py
+++ Python Programming Masterclass/Python/Squences/jukebox_menu.py
@@ -3,6 +3,13 @@ from demo import albums
 while True:
     print("Please choose your album (invalid choice exits):")
     for index, (title, artist, year, songs) in enumerate(albums):
-        print("{}: {}, {}, {}, {},"
-              .format(index + 1, title, artist, year, songs))
-    break
+        print("{}: {}"
+              .format(index + 1, title))
+    choice = int(input())
+    if 1 <= choice <= len(albums):
+        songs_list = albums[choice -1][3]
+    else:
+        break
+    print(albums[choice -1])
+    print(songs_list)
+    print()
