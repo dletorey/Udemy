0cec6f326bfc7bf91c1df64b7483bd758a429326
Dave Letorey <daveletorey@gmail.com>
2020-09-07 16:35:31 +0100

    created the first level menu of albums

--- Python Programming Masterclass/Python/Squences/jukebox_menu.py
+++ Python Programming Masterclass/Python/Squences/jukebox_menu.py
@@ -1,2 +1,8 @@
 from demo import albums
-print(albums)
+
+while True:
+    print("Please choose your album (invalid choice exits):")
+    for index, (title, artist, year, songs) in enumerate(albums):
+        print("{}: {}, {}, {}, {},"
+              .format(index + 1, title, artist, year, songs))
+    break
