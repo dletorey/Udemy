03bd443842d7f7c2f08220c0fdb1779dce33b140
Dave Letorey <daveletorey@gmail.com>
2020-09-07 12:57:11 +0100

    completed exercise 126 more unpacking

--- Python Programming Masterclass/Python/Squences/tuples_intro.py
+++ Python Programming Masterclass/Python/Squences/tuples_intro.py
@@ -1,18 +1,41 @@
 # t = ("a", "b", "c") # the brackets are optional, but it's good practise
 # print(t)
 # exercise 123 tuples are immutable
+# welcome = "Welcome to my nightmare", "Alice Cooper", 1975
+# bad = "Bad Company", "Bad Company", 1974
+# budgie = "Nightflight", "Budgie", 1981
+# imelda = "More Mayhem", "Emilda May", 2011
+# metallica = "Ride the Lightning", "Metallica", 1984
+#
+# print(metallica)
+# print(metallica[0])
+# print(metallica[1])
+# print(metallica[2])
+#
+# metallica2 = list(metallica)
+# print(metallica2)
+# metallica2[0] = "Master of Puppets"
+# print(metallica2)
+
+# Exercise 126 More Unpacking
+
 welcome = "Welcome to my nightmare", "Alice Cooper", 1975
 bad = "Bad Company", "Bad Company", 1974
 budgie = "Nightflight", "Budgie", 1981
 imelda = "More Mayhem", "Emilda May", 2011
 metallica = "Ride the Lightning", "Metallica", 1984
 
-print(metallica)
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
+# print(metallica)
+# print(metallica[0])
+# print(metallica[1])
+# print(metallica[2])
+title, artist, year = metallica
+print(title)
+print(artist)
+print(year)
 
-metallica2 = list(metallica)
-print(metallica2)
-metallica2[0] = "Master of Puppets"
-print(metallica2)
+table = ("Coffee table", 200, 100, 75, 34.50)
+print(table[1] * table[2])
+
+name, length, width, height, price = table
+print(length * width) 
