a740d7dfb82e3d23b58478246a5596223a412146
Dave Letorey <daveletorey@gmail.com>
2020-09-07 15:08:50 +0100

    completed exercise 127 nested tuples and lists

--- Python Programming Masterclass/Python/Squences/tuples_intro.py
+++ Python Programming Masterclass/Python/Squences/tuples_intro.py
@@ -1,41 +1,65 @@
-# t = ("a", "b", "c") # the brackets are optional, but it's good practise
-# print(t)
-# exercise 123 tuples are immutable
+# # t = ("a", "b", "c") # the brackets are optional, but it's good practise
+# # print(t)
+# # exercise 123 tuples are immutable
+# # welcome = "Welcome to my nightmare", "Alice Cooper", 1975
+# # bad = "Bad Company", "Bad Company", 1974
+# # budgie = "Nightflight", "Budgie", 1981
+# # imelda = "More Mayhem", "Emilda May", 2011
+# # metallica = "Ride the Lightning", "Metallica", 1984
+# #
+# # print(metallica)
+# # print(metallica[0])
+# # print(metallica[1])
+# # print(metallica[2])
+# #
+# # metallica2 = list(metallica)
+# # print(metallica2)
+# # metallica2[0] = "Master of Puppets"
+# # print(metallica2)
+#
+# # Exercise 126 More Unpacking
+#
 # welcome = "Welcome to my nightmare", "Alice Cooper", 1975
 # bad = "Bad Company", "Bad Company", 1974
 # budgie = "Nightflight", "Budgie", 1981
 # imelda = "More Mayhem", "Emilda May", 2011
 # metallica = "Ride the Lightning", "Metallica", 1984
 #
-# print(metallica)
-# print(metallica[0])
-# print(metallica[1])
-# print(metallica[2])
+# # print(metallica)
+# # print(metallica[0])
+# # print(metallica[1])
+# # print(metallica[2])
+# title, artist, year = metallica
+# print(title)
+# print(artist)
+# print(year)
 #
-# metallica2 = list(metallica)
-# print(metallica2)
-# metallica2[0] = "Master of Puppets"
-# print(metallica2)
+# table = ("Coffee table", 200, 100, 75, 34.50)
+# print(table[1] * table[2])
+#
+# name, length, width, height, price = table
+# print(length * width)
 
-# Exercise 126 More Unpacking
+# exercise 127 nested tuples and lists
 
-welcome = "Welcome to my nightmare", "Alice Cooper", 1975
-bad = "Bad Company", "Bad Company", 1974
-budgie = "Nightflight", "Budgie", 1981
-imelda = "More Mayhem", "Emilda May", 2011
-metallica = "Ride the Lightning", "Metallica", 1984
+albums = [("Welcome to my nightmare", "Alice Cooper", 1975),
+          ("Bad Company", "Bad Company", 1974),
+          ("Nightflight", "Budgie", 1981),
+          ("More Mayhem", "Imelda May", 2011),
+          ("Ride the Lightning", "Metallica", 1984),
+          ]
+print(len(albums))
+# print using index
+for album in albums:
+    print("Album: {}\tArtist: {}\tYear: {}"
+          .format(album[0],album[1],album[2]))
+# print using unpacked names
+for alb in albums:
+    album, artist, year = alb
+    print("Album: {}\tArtist: {}\tYear: {}"
+          .format(album, artist, year))
 
-# print(metallica)
-# print(metallica[0])
-# print(metallica[1])
-# print(metallica[2])
-title, artist, year = metallica
-print(title)
-print(artist)
-print(year)
-
-table = ("Coffee table", 200, 100, 75, 34.50)
-print(table[1] * table[2])
-
-name, length, width, height, price = table
-print(length * width) 
+# print unpacking tuples first
+for album, artist, year in albums:
+    print("Album: {}\tArtist: {}\tYear: {}"
+          .format(album, artist, year))
