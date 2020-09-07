2e2ce06a51240003bb58385cd3106a83e39cf83c
Dave Letorey <daveletorey@gmail.com>
2020-09-07 12:47:58 +0100

    completed exercise 125 practical uses for unpacking tuples

--- Python Programming Masterclass/Python/Squences/enumerate_example.py
+++ Python Programming Masterclass/Python/Squences/enumerate_example.py
@@ -1,2 +1,12 @@
-for index, character in enumerate("abcdefghijkl"):
-    print(index, "\t", character)
+# for index, character in enumerate("abcdefghijkl"):
+#     print(index, "\t", character)
+
+# exercise 125 practical uses for unpacking tuples
+
+for t in enumerate("abcdefghijklm"):
+    index, character = t
+    print(index, character)
+
+index, character = (0, 'a')
+print(index)
+print(character)
