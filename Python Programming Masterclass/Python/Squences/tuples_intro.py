21d3159ab2032ad55f73d92d62689eab8303a54a
Dave Letorey <daveletorey@gmail.com>
2020-09-07 12:30:19 +0100

    completed exercise 123 tuples are immutable

--- Python Programming Masterclass/Python/Squences/tuples_intro.py
+++ Python Programming Masterclass/Python/Squences/tuples_intro.py
@@ -1,2 +1,18 @@
-t = ("a", "b", "c") # the brackets are optional, but it's good practise
-print(t)
+# t = ("a", "b", "c") # the brackets are optional, but it's good practise
+# print(t)
+# exercise 123 tuples are immutable
+welcome = "Welcome to my nightmare", "Alice Cooper", 1975
+bad = "Bad Company", "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda May", 2011
+metallica = "Ride the Lightning", "Metallica", 1984
+
+print(metallica)
+print(metallica[0])
+print(metallica[1])
+print(metallica[2])
+
+metallica2 = list(metallica)
+print(metallica2)
+metallica2[0] = "Master of Puppets"
+print(metallica2)
