1b0377fb6ab6f61584f96e45acdc58d440c38fcb
Dave Letorey <daveletorey@gmail.com>
2020-09-07 16:16:31 +0100

    added a list of albums to use

---
+++ Python Programming Masterclass/Python/Squences/demo.py
@@ -0,0 +1,80 @@
+albums = [("Orbital \(aka Green\)", "Orbital", 1991,
+           [
+               (1, "The Moebius", "7:01"),
+               (2, "Speed Freak", "7:16"),
+               (3, "Oolaa", "6:21"),
+               (4, "Desert Storm", "12:05"),
+               (5, "Fahrenheit 303", "8:24"),
+               (6, "Steel Cube Idolatry", "6:34"),
+               (7, "High Rise", "8:24"),
+               (8, "Chime (Live)", "5:56"),
+               (9, "Midnight (Live)", "6:53"),
+               (10, "Belfast", "8:06"),
+               (11, "I Think It's Disgusting (Outro)", "0:51"),
+           ]
+           ),
+          ("Orbital \(aka Brown\)", "Orbital", 1991,
+           [
+               (1, "Time Becomes", "1:43"),
+               (2, "Planet of the Shapes", "9:36"),
+               (3, "Lush 3-1", "5:39"),
+               (4, "Lush 3-2", "4:40"),
+               (5, "Impact (The Earth Is Burning)", "10:27"),
+               (6, "Remind", "7:57"),
+               (7, "Walk Now...", "6:48"),
+               (8, "Monday", "7:05"),
+               (9, "Halcyon + On + On", "9:28"),
+               (10, "Input Out", "2:11"),
+           ]
+           ),
+          ("Snivilisation", "Orbital", 1994,
+           [
+               (1, "Forever", "7:59"),
+               (2, "I Wish I Had Duck Feet", "4:05"),
+               (3, "Sad But True \(featuring Alison Goldfrapp\)", "7:49"),
+               (4, "Crash and Carry", "4:43"),
+               (5, "Science Friction", "5:03"),
+               (6, "Philosophy by Numbers", "6:39"),
+               (7, "Kein Trink Wasser", "9:24"),
+               (8, "Quality Seconds", "1:25"),
+               (9, "Are We Here? \(featuring Alison Goldfrapp\)", "15:33"),
+               (10, "Attached", "12:25"),
+           ]
+           ),("Welcome to my Nightmare", "Alice Cooper", 1975,
+              [
+                  (1, "Welcome to my Nightmare"),
+                  (2, "Devil's Food"),
+                  (3, "The Black Widow"),
+                  (4, "Some Folks"),
+                  (5, "Only Women Bleed"),
+              ]
+              ),
+          ("Bad Company", "Bad Company", 1974,
+           [
+               (1, "Can't Get Enough"),
+               (2, "Rock Steady"),
+               (3, "Ready for Love"),
+               (4, "Don't Let Me Down"),
+               (5, "Bad Company"),
+               (6, "The Way I Choose"),
+               (7, "Movin' On"),
+               (8, "Seagull"),
+           ]
+           ),
+          ("Nightflight", "Budgie", 1981,
+           [
+               (1, "I Turned to Stone"),
+               (2, "Keeping a Rendezvous"),
+               (3, "Reaper of the Glory"),
+               (4, "She Used Me Up"),
+           ]
+           ),
+          ("More Mayhem", "Imelda May", 2011,
+           [
+               (1, "Pulling the Rug"),
+               (2, "Psycho"),
+               (3, "Mayhem"),
+               (4, "Kentish Town Waltz"),
+           ]
+           ),
+          ]
