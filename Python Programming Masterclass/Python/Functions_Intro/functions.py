f243de19371c9098447ea39db32dc40cd13e8768
Dave Letorey <daveletorey@gmail.com>
2020-09-15 14:41:52 +0100

    added more examples of using multiply function

--- Python Programming Masterclass/Python/Functions_Intro/functions.py
+++ Python Programming Masterclass/Python/Functions_Intro/functions.py
@@ -5,3 +5,12 @@ def multiply(x, y):
 
 answer = multiply(64736, 5)
 print(answer)
+
+forty_two = multiply(6, 7)
+print(forty_two)
+
+print("-" * 10)
+
+for val in range (1, 5):
+    two_times = multiply(2, val)
+    print(two_times)
