f1149592be1f360312b36c3c31cfed5bcfd4db33
Dave Letorey <daveletorey@gmail.com>
2020-09-15 14:57:34 +0100

    created a palindrome function

--- Python Programming Masterclass/Python/Functions_Intro/functions.py
+++ Python Programming Masterclass/Python/Functions_Intro/functions.py
@@ -2,15 +2,29 @@ def multiply(x, y):
     result = x * y
     return result
 
+def is_palindrome(string):
+    # backwards = string[::-1]
+    # return backwards == string
+    return string[::-1].casefold() == string.casefold()
 
-answer = multiply(64736, 5)
-print(answer)
 
-forty_two = multiply(6, 7)
-print(forty_two)
+# tests
+word = input("please enter a word to check: ")
+if is_palindrome(word):
+    print("'{}' is a palindrome".format(word))
+else:
+    print("'{}' is not a palindrome".format(word))
 
-print("-" * 10)
-
-for val in range (1, 5):
-    two_times = multiply(2, val)
-    print(two_times)
+# Using Multiply function
+# answer = multiply(64736, 5)
+# print(answer)
+#
+# forty_two = multiply(6, 7)
+# print(forty_two)
+#
+# print("-" * 10)
+#
+# for val in range (1, 5):
+#     two_times = multiply(2, val)
+#     print(two_times)
+#
