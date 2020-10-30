c00705658518d01c6599bd8559eb81a414c7351b
Dave Letorey <daveletorey@gmail.com>
2020-10-30 16:31:52 +0000

    imported the colorama package

--- Python Programming Masterclass/Python/Functions_Intro/colour_print2.py
+++ Python Programming Masterclass/Python/Functions_Intro/colour_print2.py
@@ -1,8 +1,6 @@
-import colorama
-
 # Some ANSI escape sequences for colours and effects
-RED = '\u001b[31m'
 BLACK = '\u001b[30m'
+RED = '\u001b[31m'
 GREEN = '\u001b[32m'
 YELLOW = '\u001b[33m'
 BLUE = '\u001b[34m'
@@ -19,15 +17,15 @@ REVERSE = '\u001b[7m'
 def colour_print(text: str, effect: str) -> None:
     """
     Print `text` using the ANSI sequences to change colour, etc
+
     :param text: The text to print.
-    :param effect: The effect we wnat. One of the constants
-        defined at the beginning of this module.
+    :param effect: The effect we want.  One of the constants
+        defined at the start of this module.
     """
     output_string = "{0}{1}{2}".format(effect, text, RESET)
     print(output_string)
 
 
-colorama.init()
 colour_print("Hello, Red", RED)
 # test that the colour was reset
 print("This should be in the default terminal colour")
@@ -37,4 +35,3 @@ colour_print("Hello, Bold", BOLD)
 colour_print("Hello, Underline", UNDERLINE)
 colour_print("Hello, Reverse", REVERSE)
 colour_print("Hello, Black", BLACK)
-colorama.deinit()
