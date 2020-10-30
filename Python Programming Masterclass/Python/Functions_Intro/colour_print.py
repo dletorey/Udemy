5bfcb2d588cd5c78743bc89a5f754fbed9a6ee51
Dave Letorey <daveletorey@gmail.com>
2020-10-30 15:13:40 +0000

    files needed for printing colour exercise

---
+++ Python Programming Masterclass/Python/Functions_Intro/colour_print.py
@@ -0,0 +1,9 @@
+colour_print("Hello, Red", RED)
+# test that the colour was reset
+print("This should be in the default terminal colour")
+colour_print("Hello, Blue", BLUE)
+colour_print("Hello, Yellow", YELLOW)
+colour_print("Hello, Bold", BOLD)
+colour_print("Hello, Underline", UNDERLINE)
+colour_print("Hello, Reverse", REVERSE)
+colour_print("Hello, Black", BLACK)
